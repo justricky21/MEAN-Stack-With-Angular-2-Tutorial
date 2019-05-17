1b5e27304035235f00187c714cb95ab3777b414e
ricardo_ruiz <rfpimpao@gmail.com>
2019-05-17 16:50:32 +0100

    Added boostrap, jquery and popper to dependencies.
    Set up routes for home and dashboard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component'
+import { DashboardComponent } from './components/dashboard/dashboard.component'
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
-  // { path "**", component" PageNotFoundComponent }
-
+  { path: '', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: "**", component: HomeComponent }
 ];
 
 @NgModule({
