031147f8e25c99a14fe07f8faf049af192489ce1
ricardo_ruiz <rfpimpao@gmail.com>
2019-05-17 09:27:11 +0100

    Added Angular

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database')
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+  if (err) {
+    console.log('Could not connect to database: ', err);
+  } else {
+    console.log('Connected to database :', config.db);
+  }
+});
 
 app.get('*', (req, res) => {
-  res.send('<h1>huehue</h1>');
+  res.send('<h1>Hello World</h1>');
 });
 
 app.listen(8080, () => {
